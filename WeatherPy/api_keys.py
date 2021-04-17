10e6cf3442d6b3dabaf18e66cd0657760ccc492a
bethgietl <bfave19@gmail.com>
2021-04-17 12:53:29 -0600

    trying to push gitignore file with my api_keys in it

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,10 @@
+# OpenWeatherMap API Key
+weather_api_key = "7111648151b6cdb2d73c65bb4671e668"
+
+
+
+# Google API Key
+g_key = "AIzaSyBNeLQSXk1Lf0TPEjrCSRrWHKryxK23Qb8"
+
+
+#citipy = "pip install citipy" pypi key = "pypi-AgEIcHlwaS5vcmcCJDA3ODYwNmRmLTU1NjAtNDBkOS1iY2M3LTEzY2IyMjc4OWY3NQACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgnmAJ72Ogbn3mN2Yv1K7-3kTPlIi8cnGvJAYXDDy0R04"
